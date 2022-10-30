4f16814c4dd0e67a16e2671fe81c6459477fe009
zhackerexploit <34616907+zhackerexploit@users.noreply.github.com>
2022-10-30 22:05:21 +0530

    Add files via upload

---
+++ Elements/css3-checkbox-styles/script.js
@@ -0,0 +1,29 @@
+// Converted to SCSS. If you want to grab just the CSS, click the `View Compiled` button on the css window over there <-- . That will list out the compiled css for you to use. Grab all the css between the comments and the html between the comments and it should work like a champ anywhere you place it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// All this jquery is just used for presentation. Not required at all for the radio buttons to work.
+$( document ).ready(function(){
+//   Hide the border by commenting out the variable below
+    var $on = 'section';
+    $($on).css({
+      'background':'none',
+      'border':'none',
+      'box-shadow':'none'
+    });
+});
